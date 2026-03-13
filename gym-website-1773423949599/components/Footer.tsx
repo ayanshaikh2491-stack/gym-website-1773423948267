954996334484c9932d9619bc6a778614abf8392b
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 23:15:51 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773423949599/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto">
+        <p className="text-center">&copy; 2023 Gym Website</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
